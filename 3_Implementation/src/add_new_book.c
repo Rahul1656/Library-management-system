21b6c212edfe4ecdef9c1d096117e4b7385574d5
256142 <anupamkg18@gmail.com>
2021-04-13 22:16:15 +0530

    fix bugs

--- 3_Implementation/src/add_new_book.c
+++ 3_Implementation/src/add_new_book.c
@@ -8,8 +8,10 @@ test_values enter_new_record(){
         return fail;
     }else{
         book_data new_book;
-        printf("Enter book id and title\n");
-        scanf("%d\n", &new_book.book_id);
+        printf("Enter book id: ");
+        scanf("%d", &new_book.book_id);
+        fflush(stdin);
+        printf("Enter book title: ");
         gets(new_book.book_title);
         strcpy(new_book.status, "Available");
         strcpy(new_book.member_name, "N/A");

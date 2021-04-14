953dc69d3d7449e9c339f31a9c075489b6e7fdc2
256142 <anupamkg18@gmail.com>
2021-04-14 14:59:09 +0530

    added testfile

--- 3_Implementation/src/update_book_status.c
+++ 3_Implementation/src/update_book_status.c
@@ -10,17 +10,7 @@
 
 test_values update_record(int id){
     char new_status[10], new_date_of_issue[10], new_due_date[10], new_member_name[10];
-    int new_member_id;
-    printf("Enter new status\n");
-    scanf("%s", new_status);
-    printf("Enter new date of issue\n");
-    scanf("%s", new_date_of_issue);
-    printf("Enter new due date\n");
-    scanf("%s", new_due_date);
-    printf("Enter member name\n");
-    scanf("%s", new_member_name);
-    printf("Enter member ID\n");
-    scanf("%d", &new_member_id);
+    int new_member_id,flag=0;
     FILE *fptr1;
     FILE *fptr2;
     fptr1 = fopen("library_data.txt","rb");
@@ -32,11 +22,22 @@ test_values update_record(int id){
         book_data *update_status=(book_data*)malloc(sizeof(book_data));
         while(fread(update_status, sizeof(book_data), 1, fptr1)!='\0'){
             if(id==update_status->book_id){
+                printf("Enter new status\n");
+                scanf("%s", new_status);
+                printf("Enter new date of issue\n");
+                scanf("%s", new_date_of_issue);
+                printf("Enter new due date\n");
+                scanf("%s", new_due_date);
+                printf("Enter member name\n");
+                scanf("%s", new_member_name);
+                printf("Enter member ID\n");
+                scanf("%d", &new_member_id);
                 strcpy(update_status->status,new_status);
                 strcpy(update_status->date_of_issue,new_date_of_issue);
                 strcpy(update_status->due_date,new_due_date);
                 strcpy(update_status->member_name,new_member_name);
                 update_status->member_id=new_member_id;
+                flag=1;
             }
             fwrite(update_status, sizeof(book_data), 1, fptr2);            
         }
@@ -45,6 +46,11 @@ test_values update_record(int id){
         free(update_status);
         remove("library_data.txt");
         rename("temp_data.txt", "library_data.txt");
-        return pass;
-    }
+        if(flag == 1){
+            return pass;
+        }else{
+            printf("Unable to find the record\n");
+            return fail;
+        }
+    }   
 }
---
+++ 3_Implementation/test/test_file.c
@@ -0,0 +1,34 @@
+#include"library_management.h"
+#include"unity_internals.h"
+#include"unity.h"
+
+void setUp(){}
+
+void tearDown(){}
+
+void test_enter_new_record(void){
+    TEST_ASSERT_EQUAL(pass,enter_new_record());
+}
+
+void test_view_a_record(void){
+    TEST_ASSERT_EQUAL(pass,view_a_record(300));
+    TEST_ASSERT_EQUAL(fail,view_a_record(-1));
+}
+
+void test_update_record(void){
+    TEST_ASSERT_EQUAL(pass,update_record(300));
+}
+
+void test_delete_record(void){
+    TEST_ASSERT_EQUAL(pass,delete_record(300));
+}
+
+int main(){
+    UNITY_BEGIN();
+
+    RUN_TEST(test_enter_new_record);
+    RUN_TEST(test_view_a_record);
+    RUN_TEST(test_update_record);
+    RUN_TEST(test_delete_record);
+    return UNITY_END();
+}
